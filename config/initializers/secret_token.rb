224a985bf2b86a87332a6a6bebaff48195d1215e
Daniel Schierbeck <dasch@zendesk.com>
2012-09-15 22:42:41 +0200

    Add generated Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GitBoard::Application.config.secret_token = '42011e4624dacaa91a0341f5f25bf507f0cefc47949e66abf6c9705bd0c0b3c1265d1f4396ddc4316feb638fe148575d2a699b08e8e473f9e56eb381f66a0f0c'
